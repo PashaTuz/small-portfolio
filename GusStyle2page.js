14ce96bf818a01189ac715f6aa64cb250869fcb1
Pasha Tuz <90028614+PashaTuz@users.noreply.github.com>
2025-06-02 17:17:33 +0200

    Update GusStyle2page.js

--- GusStyle2page.js
+++ GusStyle2page.js
@@ -1,13 +1,12 @@
-
+// Додаємо інтерактивність до елементів з класом "interactive"
 document.querySelectorAll('.interactive').forEach(item => {
     item.addEventListener('click', () => {
-        // Çì³íþºìî ôîí ³ òåêñò ïðè íàòèñêàíí³
         item.style.backgroundColor = item.style.backgroundColor === 'rgb(255, 242, 229)' ? '#ffab3d' : '#fff2e5';
         item.style.color = item.style.color === 'rgb(255, 171, 61)' ? '#333' : '#ffab3d';
     });
 });
 
-
+// Ефект анімації для центрального зображення
 const centerImage = document.querySelector('.center-image');
 if (centerImage) {
     centerImage.addEventListener('mouseenter', () => {
