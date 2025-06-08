93ee7526239091b70be1a0f67da1900b7ca1f461
Pasha Tuz <90028614+PashaTuz@users.noreply.github.com>
2025-06-08 15:44:35 +0200

    Add files via upload

---
+++ javani.js
@@ -0,0 +1,14 @@
+﻿document.querySelectorAll('.discount-btn').forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (button.id === "show-wheel-btn") return; // не відкривати зображення для колеса
+
+        const img = button.nextElementSibling;
+        if (!img || !img.getAttribute('src')) return;
+
+        const imgSrc = img.getAttribute('src');
+        const modal = document.getElementById('image-modal');
+        const modalImg = document.getElementById('modal-img');
+        modalImg.setAttribute('src', imgSrc);
+        modal.style.display = 'block';
+    });
+});
